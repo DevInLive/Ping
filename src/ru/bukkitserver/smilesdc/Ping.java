78f3ed5113389a8442ccb70404dfd7fd4ff46896
Boris <ipetrov071@gmail.com>
2012-10-12 23:50:26 +0400

    Тотальная реорганизация кода. Есть баги.

--- src/ru/bukkitserver/smilesdc/Ping.java
+++ src/ru/bukkitserver/smilesdc/Ping.java
@@ -4,6 +4,9 @@
  */
 package ru.bukkitserver.smilesdc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import net.minecraft.server.EntityPlayer;
@@ -11,30 +14,65 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
  * @author Smile
  */
-public class Ping extends JavaPlugin  {
+public class Ping extends JavaPlugin {
+    
+        public String dontHavePermission = "У вас нет прав для этого!";
+        public String yourPing = "Ваш пинг: %ping";
+        public String otherPing = "Пинг игрока %player : %ping";
+        public String playerNotOnline = "Игрок %player не онлайн!";
+        public String error = "Ошибка!";
     
         public static final Logger log = Logger.getLogger("Minecraft");
     
         public void onEnable()
-        {
-           log.info("[] Ping plugin enabel!");    
+        {                    
+            File fileConf = new File(getDataFolder(), "config.yml");
+		if(!fileConf.exists())
+                {
+		    InputStream resourceAsStream = Ping.class.getResourceAsStream("/ru/bukkitserver/smilesdc/config.yml");
+		    getDataFolder().mkdirs();
+		    try 
+                    {
+		        FileOutputStream fos = new FileOutputStream(fileConf);
+		        byte[] buff = new byte[65536];
+		        int n;
+		        while((n = resourceAsStream.read(buff)) > 0)
+                        {
+		            fos.write(buff, 0, n);
+		            fos.flush();
+		        }
+		        fos.close();
+		        buff = null;
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+		    getLogger().info("[] Сonfig loaded");
+		}
+                        FileConfiguration config = this.getConfig();
+                        this.dontHavePermission = config.getString("DontHavePermission", this.dontHavePermission);
+                        this.yourPing = config.getString("YourPing", this.yourPing);
+                        this.otherPing = config.getString("OtherPing", this.otherPing);
+                        this.playerNotOnline = config.getString("PlayerNotOnline", this.playerNotOnline);
+                        this.error = config.getString("Error", this.error);
+                        log.info("[] Ping plugin enabel!");
         }
+        
         public void onDisable()
         {
            log.info("[] Ping plugin disable!"); 
         }
-        /*
-         * Command Pping.
-         * @args Player, sender.
-         */
+        
         public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
         {
             Player p = (Player) sender;
@@ -42,41 +80,44 @@ public class Ping extends JavaPlugin  {
                             {
                                     if(!p.hasPermission("pping.getping"))
                                     {
-                                            p.sendMessage(ChatColor.RED + "У вас нет прав для этого!");
+                                            dontHavePermission = ChatColor.translateAlternateColorCodes('&',dontHavePermission);
+                                            p.sendMessage(dontHavePermission);
                                     return true;
                                     }
                                             if(args.length == 0)
                                             {
-                                                 p.sendMessage(ChatColor.GRAY + "Ваш пинг: "+((CraftPlayer)((Player)sender)).getHandle().ping);
+                                                 yourPing = ChatColor.translateAlternateColorCodes('&', yourPing);
+                                                 int ping = ((CraftPlayer)((Player)sender)).getHandle().ping;
+                                                 String pings = String.valueOf(ping);
+                                                 yourPing = yourPing.replace("%ping", pings);
+                                                 yourPing = ChatColor.translateAlternateColorCodes('&', yourPing);
+                                                 p.sendMessage(yourPing);
                                             } 
                                                 else if(args.length == 1)
                                                 {
                                                     
                                                       if(!Bukkit.getOfflinePlayer(args[0]).isOnline())
                                                     {
-                                                        sender.sendMessage(ChatColor.RED + "Игрок с ником "+ChatColor.WHITE + args[0] + ChatColor.RED + " не онлайн.");
+                                                        playerNotOnline = playerNotOnline.replace("%player", args[0]);
+                                                        playerNotOnline = ChatColor.translateAlternateColorCodes('&', playerNotOnline);
+                                                        sender.sendMessage(playerNotOnline);
                                                     return true;
                                                     }
                                                         Player player = Bukkit.getPlayer(args[0]);
-                                                        player.sendMessage(ChatColor.GRAY + "Пинг игрока "+ ChatColor.WHITE + player.getDisplayName()+ChatColor.GRAY + ": " + ((CraftPlayer)player).getHandle().ping); 
+                                                        int ping = ((CraftPlayer)player).getHandle().ping;
+                                                        String pings = String.valueOf(ping);
+                                                        otherPing = otherPing.replace("%player", player.getDisplayName());
+                                                        otherPing = otherPing.replace("%ping", pings);
+                                                        otherPing = ChatColor.translateAlternateColorCodes('&', otherPing);
+                                                        player.sendMessage(otherPing);
                                                 } 
                                                     else
                                                     {
-                                                        p.sendMessage("Ошибка!");                                                    
+                                                        error = ChatColor.translateAlternateColorCodes('&', error);
+                                                        p.sendMessage(error);                                                    
                                                     }
               return true;
                             }
               return false;
         }
-        
-        /*
-         *  Pattern's.
-         */
-        protected static Pattern chatColorPattern = Pattern.compile("(?i)&([0-9A-F])");
-	protected static Pattern chatMagicPattern = Pattern.compile("(?i)&([K])");
-	protected static Pattern chatBoldPattern = Pattern.compile("(?i)&([L])");
-	protected static Pattern chatStrikethroughPattern = Pattern.compile("(?i)&([M])");
-	protected static Pattern chatUnderlinePattern = Pattern.compile("(?i)&([N])");
-	protected static Pattern chatItalicPattern = Pattern.compile("(?i)&([O])");
-	protected static Pattern chatResetPattern = Pattern.compile("(?i)&([R])");
 }
